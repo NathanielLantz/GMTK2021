9a64e714f759d283f647278499c654bab9815a1f
AshleyCarter <45365327+ashley-amethyst@users.noreply.github.com>
2021-06-12 11:20:47 -0400

    Implemented Drag and Drop
    Drag and Drop ui elements, and drop them into slots

---
+++ GMTK2021/Assets/Scripts/Potion UI/scr_DropHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+public class scr_DropHandler : MonoBehaviour, IDropHandler
+{
+    RectTransform rectTransform;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject draggedObject = eventData.pointerDrag;
+        RectTransform draggedRectTransform = draggedObject.GetComponent<RectTransform>();
+        draggedRectTransform.anchoredPosition = rectTransform.anchoredPosition;
+        Debug.Log("OnDrop");
+    }
+}
