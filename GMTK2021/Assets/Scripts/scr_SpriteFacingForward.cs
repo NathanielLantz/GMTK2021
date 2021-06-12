c18e554609616397e54ffd2dfba723bee9166cb2
Nathan Lantz <nathanieljlantz@gmail.com>
2021-06-12 10:19:14 -0400

    Sprite Face Forward Script
    Sprite Face Forward Script Completed
    Added onto Fire Shader for testing.

--- GMTK2021/Assets/Scripts/scr_SpriteFacingForward.cs
+++ GMTK2021/Assets/Scripts/scr_SpriteFacingForward.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class scr_SpriteFacingForward : MonoBehaviour
 {
+    private Camera playerCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCamera = Camera.main;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.LookAt(playerCamera.transform);
+
+        //comment this line out if you want objects to rotate on the y axis.
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
     }
 }
