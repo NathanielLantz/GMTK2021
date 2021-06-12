73a3840f38b367b6d0aa8f30331884217486fe65
AshleyCarter <45365327+ashley-amethyst@users.noreply.github.com>
2021-06-12 18:22:31 -0400

    Potion Book changing pages implemented

---
+++ GMTK2021/Assets/Scripts/PotionBook/PotionBookController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[System.Serializable]
+public struct PotionPageContent
+{
+    public string textToDisplay;
+}
+
+public class PotionBookController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bookUI;
+
+    [SerializeField]
+    PotionPageContent[] content;
+
+
+    int currentPage;
+
+    private void Awake()
+    {
+        currentPage = 0;
+    }
+
+    [ContextMenu("Next Page")]
+    public void NextPage()
+    {
+        currentPage += 1;
+        if (currentPage > content.Length - 1)
+        {
+            currentPage = content.Length - 1;
+        }
+        UpdatePageContent();
+    }
+
+    [ContextMenu("Prevous Page")]
+    public void PreviousPage()
+    {
+        currentPage -= 1;
+        if (currentPage > content.Length - 1)
+        {
+            currentPage = content.Length - 1;
+        }
+        UpdatePageContent();
+
+    }
+
+    private void UpdatePageContent()
+    {
+        Debug.Log(content[currentPage].textToDisplay);
+    }
+
+    public void Open()
+    {
+        bookUI.SetActive(true);
+        UpdatePageContent();
+    }
+
+    public void Close()
+    {
+        bookUI.SetActive(false);
+
+    }
+}
