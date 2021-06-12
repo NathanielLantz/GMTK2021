9179b2756e2095d79cf62f662463b767f2fd3fb8
Nathan Lantz <nathanieljlantz@gmail.com>
2021-06-12 11:32:37 -0400

    Pause Menu Added
    Pause Menu script added.
    Pause Menu hooked up in game.
    May need some love.

---
+++ GMTK2021/Assets/Scripts/Menu/Scr_PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    
+    public static bool PausedState = false;
+
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PausedState)
+            {
+                Resume();
+            } 
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    void Resume ()
+    {
+        Debug.Log("Game UnPaused");
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PausedState = false;
+    }
+
+    void Paused ()
+    {
+        Debug.Log("Game Paused");
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.5f;
+        PausedState = true;
+    }
+}
