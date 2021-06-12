6fcfa7f8e5ecead501fee16140b59008307f5769
Nathan Lantz <nathanieljlantz@gmail.com>
2021-06-12 11:03:19 -0400

    MainMenu Setup
    Created MainMenu Navigation Script(s)
    Created Level Scene (main scene).
    Menu Buttons Hooked up

---
+++ GMTK2021/Assets/Scripts/Scr_MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scr_MainMenu : MonoBehaviour
+{
+    public void NavigateScene(string level)
+    {
+        Debug.Log("Scene Transitioned");
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitApplication ()
+    {
+        Debug.Log("Application Ended");
+        Application.Quit();
+    }
+}
