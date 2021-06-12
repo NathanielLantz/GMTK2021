8b2ac884cbcf514dc9e198b127061c246c991e05
AshleyCarter <45365327+ashley-amethyst@users.noreply.github.com>
2021-06-12 18:23:18 -0400

    Descriptive text handler started

---
+++ GMTK2021/Assets/Scripts/Descriptive Text UI/DescriptiveTextController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DescriptiveTextController : MonoBehaviour
+{
+    [SerializeField]
+    Camera camera;
+
+    [SerializeField]
+    LayerMask targetableMask;
+
+    [SerializeField]
+    TextMeshProUGUI descriptiveText;
+
+    void Update()
+    {
+        descriptiveText.text = "";
+
+        RaycastHit hit;
+        
+        if (Physics.Raycast(camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)), out hit, 20)){
+            TargetableObject tagetableObject = hit.collider.gameObject.GetComponent<TargetableObject>();
+            if (tagetableObject != null)
+            {
+                Debug.Log(tagetableObject.Ingredient.Name);
+                descriptiveText.text = tagetableObject.Ingredient.Name;
+            }
+        }
+
+    }
+}
---
+++ GMTK2021/Assets/Scripts/Descriptive Text UI/TargetableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetableObject: MonoBehaviour
+{
+    [SerializeField]
+    scr_Ingredient ingredient;
+    public scr_Ingredient Ingredient;
+
+}
