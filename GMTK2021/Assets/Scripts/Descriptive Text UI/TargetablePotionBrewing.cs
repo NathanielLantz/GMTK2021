01fe1893e1a838aff2dda851d61d20dc6ace075e
AshleyCarter <45365327+ashley-amethyst@users.noreply.github.com>
2021-06-13 10:55:35 -0400

    Implemented Pick up and targeting

---
+++ GMTK2021/Assets/Scripts/Descriptive Text UI/TargetablePotionBrewing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetablePotionBrewing : MonoBehaviour, TargetableObject
+{
+    [SerializeField]
+    scr_PotionManager potionManager;
+
+    public string DescriptiveText => "Brew Potion";
+
+    public void Interact(DescriptiveTextController controller)
+    {
+        scr_PotionUI.Instance.SetNextIngredient(controller.CurrentIngredient);
+        scr_PotionUI.Instance.ShowPotionUI();
+    }
+
+}
